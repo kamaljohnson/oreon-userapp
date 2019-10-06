a33875fb91d9416a212cbf2829170f74ee9496da
kamaljohnson <kamaljohnson12345@gmail.com>
2019-10-06 11:16:24 +0530

    button click inventory list

--- app/src/main/java/com/xborg/vendx/ItemAdapter.kt
+++ app/src/main/java/com/xborg/vendx/ItemAdapter.kt
@@ -1,13 +1,18 @@
 package com.xborg.vendx
 
 import android.content.Context
+import android.service.autofill.OnClickAction
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.inventory_item.view.*
 
+private var TAG = "ItemAdapter"
+
 class ItemAdapter(val items : ArrayList<Item>, val context: Context) : RecyclerView.Adapter<ItemViewHolder>() {
+
 
     // Gets the number of animals in the list
     override fun getItemCount(): Int {
@@ -22,7 +27,7 @@ class ItemAdapter(val items : ArrayList<Item>, val context: Context) : RecyclerV
     // Binds each animal in the ArrayList to a view
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         if (holder.name != null) holder.name.text = items[position].name
-        if (holder.cost != null) holder.cost.text = items[position].cost + " Rs"
+        if (holder.cost != null) holder.cost.text = "₹ " + items[position].cost
         if (holder.quantity != null) holder.quantity.text = items[position].quantity
         if (holder.items_left != null) {
             if(items[position].items_left == "") {
@@ -31,6 +36,7 @@ class ItemAdapter(val items : ArrayList<Item>, val context: Context) : RecyclerV
                     holder.items_left.text = items[position].items_left + " left"
             }
         }
+
     }
 }
 
@@ -39,6 +45,19 @@ class ItemViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     var cost = view.cost
     var quantity = view.quantity
     var items_left = view.items_left
+
+    var add_button = view.add_button
+    var remove_button = view.remove_button
+
+    init {
+        add_button.setOnClickListener{
+            Log.d(TAG, "add button clicked")
+        }
+
+        remove_button.setOnClickListener{
+            Log.d(TAG, "remove button clicked")
+        }
+    }
 }
 
 class Item {
