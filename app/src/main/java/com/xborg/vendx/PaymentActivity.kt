20ac2bae5d1832af9114fe7575fadbc95eb5f8d1
kamaljohnson <kamaljohnson12345@gmail.com>
2019-10-10 10:00:02 +0530

    3rd party pay with

--- app/src/main/java/com/xborg/vendx/PaymentActivity.kt
+++ app/src/main/java/com/xborg/vendx/PaymentActivity.kt
@@ -1,7 +1,10 @@
 package com.xborg.vendx
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -18,7 +21,7 @@ class PaymentActivity : AppCompatActivity() {
     private var TAG = "PaymentActivity"
     val db = FirebaseFirestore.getInstance()
 
-    var bank_upi_id: String = ""
+    var bank_upi_id: String = "7012043162@ybl"
     val UPI_PAYMENT:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +32,11 @@ class PaymentActivity : AppCompatActivity() {
 
         order_id_text.text = order_id
         amount_text.text = calculateBill().toString()
+
+
+        pay_button.setOnClickListener{
+            payUsingUpi(amount = amount_text.text.toString(), upiId = bank_upi_id, name = "kamal", note = "napkin payment")
+        }
     }
 
     private fun calculateBill() : Float {
@@ -41,5 +49,103 @@ class PaymentActivity : AppCompatActivity() {
             i+=1
         }
         return bill
+    }
+
+    fun payUsingUpi(amount:String, upiId:String, name:String, note:String) {
+
+        var uri: Uri = Uri.parse("upi://pay").buildUpon()
+            .appendQueryParameter("pa", upiId)
+            .appendQueryParameter("pn", name)
+            .appendQueryParameter("tn", note)
+            .appendQueryParameter("am", amount)
+            .appendQueryParameter("cu", "INR")
+            .build()
+
+        val upiPayIntent : Intent = Intent(Intent.ACTION_VIEW)
+        upiPayIntent.setData(uri)
+        val chooser:Intent = Intent.createChooser(upiPayIntent, "Pay with")
+
+        if(chooser.resolveActivity(packageManager) != null){
+            startActivityForResult(chooser, UPI_PAYMENT)
+        } else {
+            Toast.makeText(this, "No UPI app found, please install one to continue", Toast.LENGTH_LONG).show()
+        }
+    }
+    //D/UPI: onActivityResult: txnId=AXI23ae9611d45f4c9ca6d0fdcfdb61ee6f&responseCode=00&Status=SUCCESS&txnRef=912414555047
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("UPI", resultCode.toString()) //when user simply back without payment
+        if (Activity.RESULT_OK == resultCode || resultCode === 11 || requestCode == 777) {
+            if (data != null) {
+                val trxt = data.getStringExtra("response")
+                Log.d("UPI", "onActivityResult: $trxt")
+                val dataList = ArrayList<String>()
+                dataList.add(trxt)
+                upiPaymentDataOperation(dataList)
+            } else {
+                Log.d("UPI", "onActivityResult: " + "Return data is null")
+                val dataList = ArrayList<String>()
+                dataList.add("nothing")
+                upiPaymentDataOperation(dataList)
+            }
+        } else {
+            Log.d("UPI", "onActivityResult: " + "Return data is null") //when user simply back without payment
+            val dataList = ArrayList<String>()
+            dataList.add("nothing")
+            upiPaymentDataOperation(dataList)
+        }
+    }
+
+    private fun upiPaymentDataOperation(data: ArrayList<String>) {
+        if (isConnectionAvailable(this@PaymentActivity)) {
+            var str = data[0]
+            Log.d("UPIPAY", "upiPaymentDataOperation: $str")
+            var paymentCancel = ""
+            if (str == null) str = "discard"
+            var status = ""
+            var approvalRefNo = ""
+            val response = str.split("&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            for (i in response.indices) {
+                val equalStr = response[i].split("=".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                if (equalStr.size >= 2) {
+                    if (equalStr[0].toLowerCase() == "Status".toLowerCase()) {
+                        status = equalStr[1].toLowerCase()
+                    } else if (equalStr[0].toLowerCase() == "ApprovalRefNo".toLowerCase() || equalStr[0].toLowerCase() == "txnRef".toLowerCase()) {
+                        approvalRefNo = equalStr[1]
+                    }
+                } else {
+                    paymentCancel = "Payment cancelled by user."
+                }
+            }
+
+            if (status == "success") {
+                //Code to handle successful transaction here.
+                Toast.makeText(this@PaymentActivity, "Transaction successful.", Toast.LENGTH_SHORT).show()
+            } else if ("Payment cancelled by user." == paymentCancel) {
+                Toast.makeText(this@PaymentActivity, "Payment cancelled by user.", Toast.LENGTH_SHORT).show()
+            } else {
+
+            }
+        } else {
+            Toast.makeText(
+                this@PaymentActivity,
+                "Internet connection is not available. Please check and try again",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    fun isConnectionAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (connectivityManager != null) {
+            val netInfo = connectivityManager.activeNetworkInfo
+            if (netInfo != null && netInfo.isConnected
+                && netInfo.isConnectedOrConnecting
+                && netInfo.isAvailable
+            ) {
+                return true
+            }
+        }
+        return false
     }
 }
