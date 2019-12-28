4277f7023bdbbe8fcf238069888a07acdb3b7dda
kamaljohnson <kamaljohnson12345@gmail.com>
2019-12-28 10:28:35 +0530

    init payment activity

---
+++ app/src/main/java/com/xborg/vendx/activities/paymentActivity/PaymentActivity.kt
@@ -0,0 +1,13 @@
+package com.xborg.vendx.activities.paymentActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xborg.vendx.R
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
