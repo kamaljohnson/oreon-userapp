ca939935bba72a5a26b8ef99443aaaee45c4097f
kamaljohnson <kamaljohnson12345@gmail.com>
2020-01-01 03:02:18 +0530

    updated payment status with ui and icons

--- app/src/main/java/com/xborg/vendx/activities/paymentActivity/fragments/paymentStatus/PaymentStatusFragment.kt
+++ app/src/main/java/com/xborg/vendx/activities/paymentActivity/fragments/paymentStatus/PaymentStatusFragment.kt
@@ -1,20 +1,24 @@
 package com.xborg.vendx.activities.paymentActivity.fragments.paymentStatus
 
 import android.os.Bundle
+import android.os.TokenWatcher
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.xborg.vendx.R
 import com.xborg.vendx.activities.paymentActivity.SharedViewModel
 import com.xborg.vendx.database.PaymentState
+import com.xborg.vendx.database.PaymentStatus
 import kotlinx.android.synthetic.main.fragment_payment_status.*
 
 const val TAG = "PaymentStatusFragment"
 
-class PaymentStatusFragment : Fragment(){
+class PaymentStatusFragment : Fragment(), View.OnKeyListener{
 
     private lateinit var sharedViewModel: SharedViewModel
     private lateinit var viewModel: PaymentStatusViewModel
@@ -57,7 +61,57 @@ class PaymentStatusFragment : Fragment(){
                 sharedViewModel.order.value = viewModel.order.value
                 sharedViewModel.paymentState.value = updatedPaymentState
                 payment_status_text.text = viewModel.payment.value!!.status.toString()
+                when (viewModel.payment.value!!.status) {
+                    PaymentStatus.Successful -> onPaymentSuccessful()
+                    PaymentStatus.Failed -> onPaymentFailed()
+                }
             }
         })
+    }
+
+    private fun onPaymentSuccessful() {
+        status_icon.setBackgroundResource(R.drawable.success)
+        on_success_layout.visibility = View.VISIBLE
+    }
+
+    private fun onPaymentFailed() {
+        status_icon.setBackgroundResource(R.drawable.error)
+        on_failure_layout.visibility = View.VISIBLE
+    }
+
+    // region Handling User Events
+    override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+        if(keyCode == KeyEvent.KEYCODE_BACK) {
+            when(viewModel.paymentState.value) {
+                PaymentState.None -> TODO()
+                PaymentState.OrderInit -> TODO()
+                PaymentState.OrderIdReceived -> return declineEventToast()
+                PaymentState.PaymentInit -> return declineEventToast()
+                PaymentState.PaymentDone -> return declineEventToast()
+                PaymentState.PaymentTokenCreated -> return declineEventToast()
+                PaymentState.PaymentPosted -> return declineEventToast()
+                PaymentState.PaymentFinished -> {
+                    when(viewModel.payment.value!!.status) {
+                        PaymentStatus.Successful -> return proceedToMainActivity()
+                        PaymentStatus.Failed -> return proceedToNewPayment()
+                    }
+                }
+            }
+        }
+        return true
+    }
+    // endregion
+
+    private fun declineEventToast(): Boolean {
+        Toast.makeText(context, "Your transaction is processing, termination will lead to failure", Toast.LENGTH_LONG).show()
+        return false
+    }
+
+    private fun proceedToNewPayment(): Boolean {
+        return false
+    }
+
+    private fun proceedToMainActivity(): Boolean {
+        return false
     }
 }
