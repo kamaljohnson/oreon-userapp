793c96020e25c10e9f8cb8f88b7455e37442abdd
kamaljohnson <kamaljohnson12345@gmail.com>
2020-01-01 18:58:05 +0530

    init connection files

---
+++ app/src/main/java/com/xborg/vendx/activities/deviceConnectorActivity/DeviceConnectorActivity.kt
@@ -0,0 +1,13 @@
+package com.xborg.vendx.activities.deviceConnectorActivity
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.xborg.vendx.R
+
+class DeviceConnectorActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_device_connector)
+    }
+}
---
+++ app/src/main/java/com/xborg/vendx/activities/deviceConnectorActivity/fragments/prerequisites/Prerequisites.kt
@@ -0,0 +1,2 @@
+package com.xborg.vendx.activities.deviceConnectorActivity.fragments.prerequisites
+
