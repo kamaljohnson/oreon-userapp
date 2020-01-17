6e5884fb368da99466171d01736185ce48a1a798
kamaljohnson <kamaljohnson12345@gmail.com>
2020-01-17 14:55:26 +0530

    ADD onBackPress handle vendingActivity

--- app/src/main/java/com/xborg/vendx/activities/vendingActivity/VendingActivity.kt
+++ app/src/main/java/com/xborg/vendx/activities/vendingActivity/VendingActivity.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentManager
@@ -18,6 +19,7 @@ import com.xborg.vendx.activities.feedbackActivity.FeedbackActivity
 import com.xborg.vendx.activities.mainActivity.MainActivity
 import com.xborg.vendx.activities.vendingActivity.fragments.deviceCommunicator.DeviceCommunicatorFragment
 import com.xborg.vendx.activities.vendingActivity.fragments.status.VendingStatusFragment
+import com.xborg.vendx.database.VendingState
 
 private var TAG = "VendingActivity"
 
@@ -54,14 +56,27 @@ class VendingActivity : FragmentActivity() {
         fragmentTransaction.commit()
     }
 
+
     override fun onBackPressed() {
-        super.onBackPressed()
-        //TODO: do according to vending status
+        when (sharedViewModel.vendState.value) {
+            VendingState.VendingComplete -> proceedToHome()
+            else -> {
+                Toast.makeText(
+                    this,
+                    "Event Blocked, Processing Request",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun proceedToHome() {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
+
 
     private fun showFeedback() {
         val intent = Intent(this, FeedbackActivity::class.java)
