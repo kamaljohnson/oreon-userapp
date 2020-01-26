afc4b55d3afee0b684ac43ed5a0f0009594e4914
kamaljohnson <kamaljohnson12345@gmail.com>
2020-01-26 11:15:10 +0530

    Disable minify bug in serialization

--- app/src/main/java/com/xborg/vendx/database/Item.kt
+++ app/src/main/java/com/xborg/vendx/database/Item.kt
@@ -2,7 +2,7 @@ package com.xborg.vendx.database
 
 import com.google.gson.annotations.SerializedName
 
-enum class ItemCategory {
+enum class Category {
     @SerializedName("Snack")  Snack,
     @SerializedName("Beverage")  Beverage,
     @SerializedName("FastFood")  FastFood,
@@ -14,14 +14,14 @@ data class Item(
     @SerializedName("Name")  var Name: String,
     @SerializedName("Cost")  var Cost: Long,
     @SerializedName("Image")  var ImgScrUrl: String,
-    @SerializedName("Category")  var Category: ItemCategory,
+    @SerializedName("Category")  var Category: Category,
 
     @SerializedName("InShelf")  var InShelf: Boolean = false,
     @SerializedName("InMachine")  var InMachine: Boolean = false,
     @SerializedName("RemainingInMachine")  var RemainingInMachine: Int = -1,
     @SerializedName("RemainingInShelf")  var RemainingInShelf: Int = -1,
 
-    @SerializedName("cartCount")  var cartCount: Int = 0
+    @SerializedName("CartCount")  var cartCount: Int = 0
 ) {
     override fun toString(): String {
         return "Item(id='$Id', name='$Name', inShelf=$InShelf, inMachine=$InMachine, remainingInMachine=$RemainingInMachine, remainingInShelf=$RemainingInShelf)"
