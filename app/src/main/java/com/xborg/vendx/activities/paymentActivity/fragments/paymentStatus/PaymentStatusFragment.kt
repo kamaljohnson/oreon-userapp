218a9314d8394dabae60d09bace2d42212fda279
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-02-01 14:00:22 +0530

    Update app icon, Add Payment cancel

--- app/src/main/java/com/xborg/vendx/activities/paymentActivity/fragments/paymentStatus/PaymentStatusFragment.kt
+++ app/src/main/java/com/xborg/vendx/activities/paymentActivity/fragments/paymentStatus/PaymentStatusFragment.kt
@@ -1,5 +1,6 @@
 package com.xborg.vendx.activities.paymentActivity.fragments.paymentStatus
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.xborg.vendx.R
+import com.xborg.vendx.activities.mainActivity.MainActivity
 import com.xborg.vendx.activities.paymentActivity.SharedViewModel
 import com.xborg.vendx.activities.vendingActivity.VendingActivity
 import com.xborg.vendx.database.PaymentState
@@ -41,8 +43,12 @@ class PaymentStatusFragment : Fragment() {
         retry_button.setOnClickListener {
             initPaymentRetry()
         }
+        cancel_button.setOnClickListener {
+            proceedToHome()
+        }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun observerSharedViewModel() {
 
         sharedViewModel = ViewModelProviders.of(activity!!).get(SharedViewModel::class.java)
@@ -66,7 +72,7 @@ class PaymentStatusFragment : Fragment() {
                 sharedViewModel.payment.value = viewModel.payment.value
                 sharedViewModel.order.value = viewModel.order.value
                 sharedViewModel.paymentState.value = updatedPaymentState
-                payment_status_text.text = viewModel.payment.value!!.Status.toString()
+                payment_status_text.text = "Payment " + viewModel.payment.value!!.Status.toString()
                 when (viewModel.payment.value!!.Status) {
                     PaymentStatus.Successful -> onPaymentSuccessful()
                     PaymentStatus.Failed -> onPaymentFailed()
@@ -92,6 +98,13 @@ class PaymentStatusFragment : Fragment() {
 
     private fun proceedToVending() {
         val intent = Intent(context, VendingActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
+
+    private fun proceedToHome() {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
