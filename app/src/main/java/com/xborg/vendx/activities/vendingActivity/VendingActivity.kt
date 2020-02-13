b34313f3597a6a2b08d1de118af34581daf8e0a0
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-02-13 23:36:42 +0530

    Generating QR code

--- app/src/main/java/com/xborg/vendx/activities/vendingActivity/VendingActivity.kt
+++ app/src/main/java/com/xborg/vendx/activities/vendingActivity/VendingActivity.kt
@@ -1,9 +1,16 @@
 package com.xborg.vendx.activities.vendingActivity
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.xborg.vendx.R
+import kotlinx.android.synthetic.main.activity_vending.*
 
 private var TAG = "VendingActivity"
 
@@ -16,6 +23,17 @@ class VendingActivity : FragmentActivity() {
         setContentView(R.layout.activity_vending)
 
         sharedViewModel = ViewModelProviders.of(this).get(SharedViewModel::class.java)
+
+        val text ="Test QR code generated" // Whatever you need to encode in the QR code
+        val multiFormatWriter = MultiFormatWriter()
+        try {
+            val bitMatrix: BitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap: Bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            qr_code_view.setImageBitmap(bitmap)
+        } catch (e: WriterException) {
+            e.printStackTrace();
+        }
     }
 
 }
