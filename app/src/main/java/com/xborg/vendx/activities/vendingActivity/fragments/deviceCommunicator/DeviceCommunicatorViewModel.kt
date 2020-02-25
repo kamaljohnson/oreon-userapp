d3a1d7572d54c7305e38ddc1ef216907cb0008d3
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-02-25 15:08:55 +0530

    Add connector & service files for ble connection

--- app/src/main/java/com/xborg/vendx/activities/vendingActivity/fragments/deviceCommunicator/DeviceCommunicatorViewModel.kt
+++ app/src/main/java/com/xborg/vendx/activities/vendingActivity/fragments/deviceCommunicator/DeviceCommunicatorViewModel.kt
@@ -1,7 +1,11 @@
 package com.xborg.vendx.activities.vendingActivity.fragments.deviceCommunicator
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.xborg.vendx.database.Machine
 
 class DeviceCommunicatorViewModel : ViewModel() {
+
+    val selectedMachine = MutableLiveData<Machine>()        //machine selected for vending
 
 }
