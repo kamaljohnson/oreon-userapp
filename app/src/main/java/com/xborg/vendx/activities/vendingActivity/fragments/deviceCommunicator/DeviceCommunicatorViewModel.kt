2ba59cc1dcb4c47397d691a4a422cc6e69dd102c
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-03-23 14:41:43 +0530

    :fix: bluetooth connection works fine for now, needs update in future

--- app/src/main/java/com/xborg/vendx/activities/vendingActivity/fragments/deviceCommunicator/DeviceCommunicatorViewModel.kt
+++ app/src/main/java/com/xborg/vendx/activities/vendingActivity/fragments/deviceCommunicator/DeviceCommunicatorViewModel.kt
@@ -1,11 +1,7 @@
 package com.xborg.vendx.activities.vendingActivity.fragments.deviceCommunicator
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.xborg.vendx.database.Machine
 
 class DeviceCommunicatorViewModel : ViewModel() {
-
-    val selectedMachine = MutableLiveData<Machine>()        //machine selected for vending
 
 }
