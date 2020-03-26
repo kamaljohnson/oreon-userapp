6cdbd9c1d84fc65e3f900cf15909954a02005a2c
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-03-26 21:45:18 +0530

    :art: Update feedback model field topic from enum choice

--- app/src/main/java/com/xborg/vendx/database/Feedback.kt
+++ app/src/main/java/com/xborg/vendx/database/Feedback.kt
@@ -2,9 +2,16 @@ package com.xborg.vendx.database
 
 import com.google.gson.annotations.SerializedName
 
+enum class FeedbackTopic {
+    @SerializedName("UI") UI,
+    @SerializedName("VE") Vending,
+    @SerializedName("VU") Vulnerability,
+    @SerializedName("PY") Payment,
+    @SerializedName("OT") Other,
+}
 
 data class Feedback(
     @SerializedName("user")  var User: String = "",
-    @SerializedName("topic")  var Topic: String = "",
+    @SerializedName("topic")  var Topic: FeedbackTopic,
     @SerializedName("message")  var Message: String = ""
 )
