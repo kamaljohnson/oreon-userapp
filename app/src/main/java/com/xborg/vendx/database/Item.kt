26d2d484f5520259a2e42af85192d47a4f8c3d24
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-03-29 14:01:35 +0530

    :fire: Remove Category data class
    :art: Change category data type to string

--- app/src/main/java/com/xborg/vendx/database/Item.kt
+++ app/src/main/java/com/xborg/vendx/database/Item.kt
@@ -2,20 +2,12 @@ package com.xborg.vendx.database
 
 import com.google.gson.annotations.SerializedName
 
-data class Category(
-    @SerializedName("name") var Name: String
-) {
-    override fun toString(): String {
-        return "category='$Name'"
-    }
-}
-
 data class ItemDetail(
     @SerializedName("id") var Id: String,
     @SerializedName("name") var Name: String,
     @SerializedName("cost") var Cost: Long,
     @SerializedName("discounted_cost") var DiscountedCost: Long,
-    @SerializedName("category") var Category: Category,
+    @SerializedName("category") var Category: String,
     @SerializedName("foreground_asset") var ForegroundAsset: String,
     @SerializedName("background_asset") var BackgroundAsset: String,
     @SerializedName("content_asset") var ContentAsset: String
