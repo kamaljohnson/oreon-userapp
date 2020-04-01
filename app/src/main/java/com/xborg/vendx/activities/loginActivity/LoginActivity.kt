b384d77323dd50760e37eff66ec1514a88fec21d
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-04-01 12:56:06 +0530

    :art: Add facebook login - receive access token

--- app/src/main/java/com/xborg/vendx/activities/loginActivity/LoginActivity.kt
+++ app/src/main/java/com/xborg/vendx/activities/loginActivity/LoginActivity.kt
@@ -1,16 +1,19 @@
 package com.xborg.vendx.activities.loginActivity
 
+//import com.xborg.vendx.activities.mainActivity.MainActivity
+
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.facebook.AccessToken
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -18,8 +21,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.xborg.vendx.R
-//import com.xborg.vendx.activities.mainActivity.MainActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
 
 
 private var TAG = "LoginActivity"
@@ -28,12 +31,12 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var sharedViewModel: SharedViewModel
 
-    private lateinit var callbackManager: CallbackManager
-    private final var facebookEmail:String = "email"
-    private var facebookAccessToken = AccessToken.getCurrentAccessToken()
+    //Facebook Login
+    var callbackManager: CallbackManager = CallbackManager.Factory.create();
 
-    var isLoggedIn = facebookAccessToken != null && !facebookAccessToken!!.isExpired
+    val EMAIL = "email"
 
+    //Google Login
     lateinit var mGoogleSignInClient: GoogleSignInClient
     private val GOOGLE_SIGNIN = 9001
 
@@ -44,29 +47,42 @@ class LoginActivity : AppCompatActivity() {
         sharedViewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
 
 //        region Facebook Login
-        facebook_login_button.setOnClickListener {
-            Log.i(TAG, "facebook login button clicked")
 
-            callbackManager = CallbackManager.Factory.create()
-            LoginManager.getInstance().logInWithReadPermissions(this, listOf("public_profile", "email"))
-            LoginManager.getInstance().registerCallback(callbackManager,
-                object : FacebookCallback<LoginResult> {
-                    override fun onSuccess(loginResult: LoginResult) {
-                        Log.d("MainActivity", "Facebook token: " + loginResult.accessToken.token)
-//                        startActivity(Intent(applicationContext, MainActivity::class.java))
-                    }
+        login_button.setReadPermissions(Arrays.asList(EMAIL))
 
-                    override fun onCancel() {
-                        Log.d("MainActivity", "Facebook onCancel.")
+        // Callback registration
+        login_button.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+            override fun onSuccess(loginResult: LoginResult?) {
+                // App code
+            }
 
-                    }
+            override fun onCancel() {
+                // App code
+            }
 
-                    override fun onError(error: FacebookException) {
-                        Log.d("MainActivity", "Facebook onError.")
+            override fun onError(exception: FacebookException) {
+                // App code
+            }
+        })
 
-                    }
-                })
-        }
+        callbackManager = CallbackManager.Factory.create()
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {
+                    Log.i(TAG, "Access Token : " + loginResult!!.accessToken.token)
+                    sharedViewModel.sendFacebookTokenId(loginResult.accessToken.token)
+                }
+
+                override fun onCancel() {
+                    Log.i(TAG, "Login cancelled")
+                }
+
+                override fun onError(exception: FacebookException) {
+                    Log.i(TAG, "Login Error: $exception")
+                }
+            })
+
 //        endregion
 //        region Google Login
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -110,7 +126,7 @@ class LoginActivity : AppCompatActivity() {
         resultCode: Int,
         data: Intent?
     ) {
-//        callbackManager.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == GOOGLE_SIGNIN) {
