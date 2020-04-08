4382a45f4e5391d364cb5bb55e596ba18a0844ba
Kamal Johnson <kamaljohnson12345@gmail.com>
2020-04-08 19:41:23 +0530

    :lipstick: Add init chat ui elements

--- app/src/main/java/com/xborg/vendx/activities/customerSupportActivity/fragments/chat/ChatFragment.kt
+++ app/src/main/java/com/xborg/vendx/activities/customerSupportActivity/fragments/chat/ChatFragment.kt
@@ -20,7 +20,7 @@ class ChatFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.customer_support_fragment, container, false)
+        return inflater.inflate(R.layout.chat_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
